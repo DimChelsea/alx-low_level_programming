9ff2af9e690877196bbeae147e51f5fde1e23bb6
DimChelsea <chelseadim14@gmail.com>
2023-05-24 02:21:28 -0700

    Task 0.

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * binary_to_uint- converts a binary number to an unsigned integer
+ * @b: points to a string of 0 and 1 characters.
+ * Return: converted number.
+ * NULL if string does not  contain 0 or 1.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0, length;
+	unsigned int Numconv = 0;
+
+	for (length = 0; b[length] != 0; ++length)
+
+	if (b == NULL)
+		return (0);
+	length--;
+	while (length >= 0)
+	{
+		if (b[i]  == 48)
+		{
+			Numconv += (0 * (1 << length));
+		}
+		else if (b[i] == 49)
+		{
+			Numconv += (1 * (1 << length));
+		}
+		else
+			return (0);
+		length++;
+		i++;
+	}
+	return (Numconv);
+}
